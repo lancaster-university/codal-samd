d31908627cc4ca55224fadbbe96c7044bbe1b200
Michal Moskal <michal@moskal.me>
2018-11-14 16:34:11 -0800

    Working on clocks

--- src/SAMDDAC.cpp
+++ src/SAMDDAC.cpp
@@ -39,6 +39,7 @@ DEALINGS IN THE SOFTWARE.
 
 extern "C"
 {
+#include "clocks.h"
 #include "timers.h"
 }
 
@@ -90,11 +91,11 @@ SAMDDAC::SAMDDAC(ZPin &pin, DataSource &source, int sampleRate, uint16_t id) : u
 #ifdef SAMD21
     DAC->EVCTRL.reg |= DAC_EVCTRL_STARTEI;
     // We disable the voltage pump because we always run at 3.3v.
-    DAC->CTRLB.reg = DAC_CTRLB_REFSEL_AVCC | DAC_CTRLB_LEFTADJ | DAC_CTRLB_EOEN | DAC_CTRLB_VPD;
+    DAC->CTRLB.reg = DAC_CTRLB_REFSEL_AVCC | DAC_CTRLB_EOEN | DAC_CTRLB_VPD; //  | DAC_CTRLB_LEFTADJ
 #endif
 #ifdef SAMD51
     DAC->EVCTRL.reg |= DAC_EVCTRL_STARTEI0;
-    DAC->DACCTRL[0].reg = DAC_DACCTRL_CCTRL_CC1M | DAC_DACCTRL_ENABLE | DAC_DACCTRL_LEFTADJ;
+    DAC->DACCTRL[0].reg = DAC_DACCTRL_CCTRL_CC1M | DAC_DACCTRL_ENABLE; // | DAC_DACCTRL_LEFTADJ;
     DAC->CTRLB.reg = DAC_CTRLB_REFSEL_VREFPU;
 #endif
 
@@ -112,11 +113,11 @@ SAMDDAC::SAMDDAC(ZPin &pin, DataSource &source, int sampleRate, uint16_t id) : u
 #endif
 
     // Use a timer to coordinate when DAC conversions occur.
-    Tc *t = TC3;
+    this->tc = TC3;
     uint8_t tc_index = 0;
     while (tc_index < TC_INST_NUM)
     {
-        if (tc_insts[tc_index] == t)
+        if (tc_insts[tc_index] == this->tc)
             break;
         tc_index++;
     }
@@ -128,58 +129,30 @@ SAMDDAC::SAMDDAC(ZPin &pin, DataSource &source, int sampleRate, uint16_t id) : u
     tc_gclk = 1;
 #endif
 
+    tc_set_enable(tc, false);
     turn_on_clocks(true, tc_index, tc_gclk);
 
-    DMESG("clock: %d %d", tc_index, tc_gclk);
-
     // Don't bother setting the period. We set it before you playback anything.
-    tc_set_enable(t, false);
-    // tc_reset(t);
+    tc_set_enable(tc, false);
+    tc_reset(tc);
 
-    t->COUNT16.CTRLA.bit.MODE = 0;
+    tc->COUNT16.CTRLA.bit.MODE = 0;
 #ifdef SAMD51
-    t->COUNT16.WAVE.reg = TC_WAVE_WAVEGEN_MFRQ;
+    tc->COUNT16.WAVE.reg = TC_WAVE_WAVEGEN_MFRQ;
 #endif
 #ifdef SAMD21
-    t->COUNT16.CTRLA.bit.WAVEGEN = TC_CTRLA_WAVEGEN_MFRQ_Val;
+    tc->COUNT16.CTRLA.bit.WAVEGEN = TC_CTRLA_WAVEGEN_MFRQ_Val;
 #endif
-    t->COUNT16.CTRLA.bit.RUNSTDBY = 1;
-    t->COUNT16.CTRLA.bit.PRESCALER = 0;
-    t->COUNT16.EVCTRL.reg = TC_EVCTRL_OVFEO;
+    tc->COUNT16.CTRLA.bit.RUNSTDBY = 1;
+    tc->COUNT16.CTRLA.bit.PRESCALER = 0;
+    tc->COUNT16.EVCTRL.reg = TC_EVCTRL_OVFEO;
     // t->COUNT16.CTRLC.reg = 0x00;     // compare mode
-    t->COUNT16.CTRLBCLR.bit.DIR = 1; // count up
+    tc->COUNT16.CTRLBCLR.bit.DIR = 1; // count up
 
-    // tc_set_enable(t, true);
-    // t->COUNT16.CTRLBSET.reg = TC_CTRLBSET_CMD_STOP;
-    /*
-        {
-            CTRLA = {bit = {SWRST = 0, ENABLE = 1, MODE = 0, PRESCSYNC = 0, RUNSTDBY = 1, ONDEMAND =
-       0, PRESCALER = 0, ALOCK = 0, CAPTEN0 = 0, CAPTEN1 = 0, COPEN0 = 0, COPEN1 = 0, CAPTMODE0 = 0,
-       CAPTMODE1 = 0}, vec = {CAPTEN = 0, COPEN = 0}, reg = 66}, CTRLBCLR = {bit = {DIR = 0 '\000',
-       LUPD = 0 '\000', ONESHOT = 0 '\000', CMD = 0 '\000'}, reg = 0 '\000'}, CTRLBSET = {bit = {DIR
-       = 0 '\000', LUPD = 0 '\000', ONESHOT = 0 '\000', CMD = 0 '\000'}, reg = 0 '\000'}, EVCTRL =
-       {bit = {EVACT = 0, TCINV = 0, TCEI = 0, OVFEO = 1, MCEO0 = 0, MCEO1 = 0}, vec = {MCEO = 0},
-       reg = 256}, INTENCLR = {bit = {OVF = 0 '\000', ERR = 0 '\000', MC0 = 0 '\000', MC1 = 0
-       '\000'}, vec = {MC = 0 '\000'}, reg = 0 '\000'}, INTENSET = {bit = {OVF = 0 '\000', ERR = 0
-       '\000', MC0 = 0 '\000', MC1 = 0 '\000'}, vec = {MC = 0 '\000'}, reg = 0 '\000'}, INTFLAG =
-       {bit = {OVF = 0 '\000', ERR = 0 '\000', MC0 = 0 '\000', MC1 = 1 '\001'}, vec = {MC = 2
-       '\002'}, reg = 32 ' '}, STATUS = {bit = {STOP = 0 '\000', SLAVE = 0 '\000', PERBUFV = 0
-       '\000', CCBUFV0 = 0 '\000', CCBUFV1 = 0 '\000'}, vec = {CCBUFV = 0 '\000'}, reg = 0 '\000'},
-            WAVE = {bit = {WAVEGEN = 1 '\001'}, reg = 1 '\001'},
-            DRVCTRL = {bit = {INVEN0 = 0 '\000', INVEN1 = 0 '\000'}, vec = {INVEN = 0 '\000'},
-                       reg = 0 '\000'},
-            Reserved1 = "", DBGCTRL = {bit = {DBGRUN = 0 '\000'}, reg = 0 '\000'},
-            SYNCBUSY = {bit = {SWRST = 0, ENABLE = 0, CTRLB = 0, STATUS = 0, COUNT = 0, PER = 0,
-                               CC0 = 0, CC1 = 0},
-                        vec = {CC = 0}, reg = 0},
-            COUNT = {bit = {COUNT = 0}, reg = 0}, Reserved2 = "\000\000\000\000\000",
-            CC = {{bit = {CC = 1088}, reg = 1088}, {bit = {CC = 0}, reg = 0}},
-            Reserved3 = '\000' < repeats 15 times >, CCBUF = {
-                {bit = {CCBUF = 1088}, reg = 1088},
-                {bit = {CCBUF = 0}, reg = 0}
-            }
-        }
-    */
+    //tc_set_enable(tc, true);
+    //tc->COUNT16.CTRLBSET.reg = TC_CTRLBSET_CMD_STOP;
+
+    setSampleRate(sampleRate);
 
     pin._setMux(MUX_B);
 
@@ -190,13 +163,8 @@ SAMDDAC::SAMDDAC(ZPin &pin, DataSource &source, int sampleRate, uint16_t id) : u
     dmaInstance->onTransferComplete(this);
     dmaInstance->configure(TC3_DMAC_ID_OVF, BeatHalfWord, NULL, (volatile void *)&DAC_DATA);
 
-    setSampleRate(sampleRate);
-
     // Register with our upstream component
     source.connect(*this);
-
-    target_wait_us(50);
-    DMESG("cl: %d", TC3->COUNT16.COUNT.reg);
 }
 
 /**
@@ -225,11 +193,9 @@ int SAMDDAC::setSampleRate(int frequency)
     uint32_t period = CONF_GCLK_DAC_FREQUENCY / frequency;
     sampleRate = CONF_GCLK_DAC_FREQUENCY / period;
 
-    tc_set_enable(TC3, false);
-    TC3->COUNT16.CC[0].reg = period; // Set period
-    tc_set_enable(TC3, true);        // Restart the timer
-
-    DMESG("cl: %d", TC3->COUNT16.COUNT.reg);
+    tc_set_enable(tc, false);
+    tc->COUNT16.CC[0].reg = period; // Set period
+    tc_set_enable(tc, true);        // Restart the timer
 
     return DEVICE_OK;
 }
@@ -267,6 +233,11 @@ int SAMDDAC::pull()
     DMESG("DAC %d bytes", output.length());
 
     dmaInstance->transfer((const void *)&output[0], NULL, output.length());
+
+    DMESG("btcnt: %d", dmaInstance->getDescriptor().BTCNT.reg);
+    target_wait_us(100);
+    //dmaInstance->disable();
+    DMESG("btcnt: %d", dmaInstance->getDescriptor().BTCNT.reg);
 
     return DEVICE_OK;
 }
