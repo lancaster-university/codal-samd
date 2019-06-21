5fd92f86bdc870c5498e15c55ee21691713f06bd
Michal Moskal <michal@moskal.me>
2019-06-21 15:37:02 -0700

    Remove accidental debug

--- src/ZI2C.cpp
+++ src/ZI2C.cpp
@@ -100,7 +100,7 @@ int ZI2C::setFrequency(uint32_t frequency)
 int ZI2C::write(uint16_t address, uint8_t *data, int len, bool repeated)
 {
     address = address >> 1;
-    DMESG("W A: %d L: %d", address, len);
+    // DMESG("W A: %d L: %d", address, len);
     struct _i2c_m_msg msg;
 
     int ret = I2C_OK;
@@ -115,7 +115,7 @@ int ZI2C::write(uint16_t address, uint8_t *data, int len, bool repeated)
 
         if (ret == I2C_OK)
         {
-            DMESG("OK");
+            // DMESG("OK");
             return DEVICE_OK;
         }
     }
